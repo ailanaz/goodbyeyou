5894cb5d536b20b0883d33abe1d39abb7b86a257
ailanaz <117781743+ailanaz@users.noreply.github.com>
2026-04-09 16:17:16 -0500

    21

--- src/App.js
+++ src/App.js
@@ -1109,7 +1109,7 @@ function Navigation() {
             ))}
           </ul>
           <Link to="/" className="nav-logo">
-            <img src="/logo full.png" alt="Goodbye You" className="nav-logo-image" />
+            <img src="/logo-full-cropped.png" alt="Goodbye You" className="nav-logo-image" />
             <span className="nav-logo-tagline">Goodbye to you. Goodbye from you. Goodbye for you.</span>
           </Link>
           <ul className="nav-links nav-links-right">
